c0c97cc8404ffa6900799dd78210088cba50944c
MouhInf <mouhammad@sts.sofitrans.sn>
2026-04-25 10:01:30 +0000

    Fix: use credential.cert properly

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -36,9 +36,11 @@ export const getAdminDb = async (): Promise<Firestore | null> => {
       const privateKey = getPrivateKey();
       
       _adminApp = admin.initializeApp({
-        projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-        privateKey: privateKey,
+        credential: admin.credential.cert({
+          projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+          clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+          privateKey: privateKey,
+        }),
       });
     }
     _adminDb = _adminApp.firestore();
@@ -53,9 +55,11 @@ export const getAdminAuth = async (): Promise<Auth | null> => {
     if (!_adminApp) {
       const privateKey = getPrivateKey();
       _adminApp = admin.initializeApp({
-        projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-        privateKey: privateKey,
+        credential: admin.credential.cert({
+          projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+          clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+          privateKey: privateKey,
+        }),
       });
     }
     _adminAuth = _adminApp.auth();
